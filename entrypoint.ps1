ba10256785b69134473763e2820f77bc3af1ab33
Tyler Leonhardt <tylerl0706@gmail.com>
2020-07-14 20:03:31 -0700

    testing

--- entrypoint.ps1
+++ entrypoint.ps1
@@ -1,7 +1,10 @@
 $ProgressPreference = 'Ignore'
+$ErrorActionPreference = 'Stop'
 
-if (!$env:GITHUB_TOKEN) {
-    throw "Environment variable for GITHUB_TOKEN not found"
+Import-Module $PSScriptRoot/github.psm1
+$token = Get-GitHubInput -Name repo-token
+if (!$token) {
+    throw "repo-token not specified"
 }
 
 # Setup PowerShellForGitHub for use in GitHub Actions
@@ -11,5 +14,51 @@ Set-GitHubAuthentication -Credential $cred
 $repoArgs = $env:GITHUB_REPOSITORY -split '/'
 Set-GitHubConfiguration -DefaultOwnerName $repoArgs[0] -DefaultRepositoryName $repoArgs[1]
 
-# Put custom code after here
-Get-GitHubPullRequest | Select-Object title,number,html_url
+$context = Get-GitHubContext
+if ($context.payload.action -ne 'opened') {
+    Write-Host 'No issue or PR was opened, skipping'
+    return
+}
+
+# Do nothing if its not a pr or issue
+$isIssue = !!$context.payload.issue
+if (!$isIssue -and !$context.payload.pull_request) {
+    Write-Host 'The event that triggered this action was not a pull request or issue, skipping.'
+    return
+}
+
+# Do nothing if its not their first contribution
+Write-Host 'Checking if its the users first contribution'
+if (!$context.payload.sender) {
+    throw 'Internal error, no sender provided by GitHub'
+}
+
+$creator = $context.payload.sender.login;
+$issue = $context.issue;
+if ($isIssue) {
+    $firstContribution = isFirstIssue -Creator $creator -CurrentIssue $issue.number
+} else {
+    $firstContribution = isFirstPull -Creator $creator -CurrentPullRequest $issue.number
+}
+
+if (!$firstContribution) {
+    Write-Host 'Not the users first contribution'
+    return
+}
+
+$issueType = $isIssue ? 'issue' : 'pull request';
+
+# Add a comment to the appropriate place
+Write-Host "Adding message: $message to $issueType $($issue.number)"
+if ($isIssue) {
+    await client.issues.createComment({
+    owner: issue.owner,
+    repo: issue.repo,
+    issue_number: issue.number,
+    body: message
+    });
+
+    New-GitHubComment -Issue $issue.number -Body (Get-GitHubInput -Name issue-message)
+} else {
+    New-GitHubComment -Issue $issue.number -Body (Get-GitHubInput -Name pr-message)
+}
---
+++ github.psm1
@@ -0,0 +1,65 @@
+function Get-GitHubInput {
+    param(
+        # Name of the input
+        [Parameter(Mandatory)]
+        [string]
+        $Name
+    )
+
+    $Name = ($Name -replace " ","_").ToUpper()
+    $var = Get-Variable env:INPUT_$Name
+    if ($var) {
+        $var.Value
+    }
+}
+
+function Get-GitHubContext {
+    if ($env:GITHUB_EVENT_PATH) {
+        $payload = (Get-Content -Raw $env:GITHUB_EVENT_PATH) | ConvertFrom-Json
+    }
+
+    @{
+        EventName = $env:GITHUB_EVENT_NAME
+        Sha = $env:GITHUB_SHA
+        Ref = $env:GITHUB_REF
+        Workflow = $env:GITHUB_WORKFLOW
+        Action = $env:GITHUB_ACTION
+        Actor = $env:GITHUB_ACTOR
+        Payload = $payload
+    }
+}
+
+function isFirstIssue($creator, $CurrentIssue) {
+
+  $issues = ,(Get-GitHubIssue -Creator $creator)
+
+  if ($issues.Count === 0) {
+    return $true
+  }
+
+  foreach ($issue in $issues) {
+    if ($issue.number -lt $CurrentIssue -and !$issue.pull_request) {
+      return $false
+    }
+  }
+
+  return $true
+}
+
+# No way to filter pulls by creator
+function isFirstPull($Creator,$CurrentPullRequest) {
+  # Provide console output if we loop for a while.
+  $pulls = ,(Get-GitHubPullRequest -State All)
+
+  if ($pulls.Count === 0) {
+    return $true
+  }
+
+  foreach ($pull in $pulls) {
+    if ($pull.user.login === $creator -and $pull.number -lt $CurrentPullRequest) {
+      return $false
+    }
+  }
+}
+
+Export-ModuleMember -Function Get-Input,Get-GitHubContext,isFirstIssue,isFirstPull
